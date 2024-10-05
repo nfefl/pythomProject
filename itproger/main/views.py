5fa668366cc147942445e56609f7e07c734188f6
nfefl <fazylzynov@mail.ru>
2024-10-05 13:03:07 +0300

    Smth

--- itproger/main/views.py
+++ itproger/main/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    data = {
+        'tittle': 'Instagrame'
+    }
+    return render(request, 'index.html', data)
 def about(request):
     return render(request, 'about.html')
 
